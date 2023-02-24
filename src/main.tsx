5e03b06d8a11b721515595d63ee89ba3bf238a53
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-02-24 11:58:26 +0100

    Router added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Root from './views/Root';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
---
+++ src/views/Root.tsx
@@ -0,0 +1,26 @@
+function Root() {
+  return (
+    <div className="Root">
+      <h1>7GUIs in React</h1>
+      <ul>
+        <li>
+          <a href="/counter">Counter</a>
+        </li>
+        <li>
+          <a href="/converter">Temperature Converter</a>
+        </li>
+        <li>
+          <a href="/booker">Flight Booker</a>
+        </li>
+        <li>
+          <a href="/timer">Timer</a>
+        </li>
+        <li>
+          <a href="/crud">CRUD</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Root;
